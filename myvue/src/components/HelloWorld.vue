3e91c396884826e4d5d81723834b9c4f7e9d2185
chengpanpan <chengpanpan@bjscfl.com>
2018-02-24 18:36:43 +0800

    + vuex 个类操作

--- myvue/src/components/HelloWorld.vue
+++ myvue/src/components/HelloWorld.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="hello">
     <!--vuex -内 方法 使用-->
-    <button @click="$store.commit('jia')">+</button>
+    <button @click="jia">+</button>
     <button @click="$store.commit('jian')">-</button>
+    <button @click="jiaPlus">+Plus</button>
+    <button @click="jianPlus">-Plus</button>
     <!--vuex -内 参数 使用-->
-    {{ $store.state.count }}
+    {{ $store.state.count }} - {{ count }}
     <vue-editor v-model="content"
                 :config="editorOption"
                 @blur="onEditorBlur($event)"
@@ -15,35 +17,61 @@
 </template>
 
 <script>
-  import { VueEditor } from 'vue2-quill-editor'
-export default {
-  name: 'HelloWorld',
-  components:{VueEditor},
-  data () {
-    return {
-      content: '<h1>Some initial content</h1>',
-      editorOption:{
+  import {VueEditor} from 'vue2-quill-editor';
+  //引入状态对象的管理工具
+  import {mapState} from 'vuex';
+  //引入触发状态的管理工具
+  import {mapMutations} from 'vuex';
+  //引入Getters
+  import {mapGetters} from 'vuex';
+  //引入异步的触发状态管理工具
+  import {mapActions} from 'vuex';
 
-      }
+  export default {
+    name: 'HelloWorld' ,
+    components: {VueEditor} ,
+    data() {
+      return {
+        content: '<h1>Some initial content</h1>' ,
+        editorOption: {}
+      };
+    } ,
+    //检测 vuex 中状态对象的数据
+    // computed: mapState({
+    //   count: state => state.count
+    // })
+    //更加优化版 - 注册的时候 一定要加上 ...
+    computed: {
+      ...mapState(['count'])
+    },
+    methods:{
+      ...mapMutations(['jia']),
+      // count(){
+      //   return this.$store.getters
+      // }
+      ...mapGetters(['count']),
+      ...mapActions(['jiaPlus','jianPlus'])
     }
-  }
-}
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
 </style>
--- myvue/src/store/index.js
+++ myvue/src/store/index.js
@@ -2,20 +2,37 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
-
+//状态对象
 const state = {
   count: 4
 };
-
-const mutations={
-  jian(state){
+//触发对象（同步）
+const mutations = {
+  jian(state) {
     state.count--;
-  },
-  jia(state){
+  } ,
+  jia(state) {
     state.count++;
+  }
+};
+//计算属性，触发及计算
+const getters = {
+  count: function (state) {
+    return state.count += 100;
+  }
+};
+//触发对象（异步）
+const actions={
+  jiaPlus(context){
+    context.commit('jia',{a:1})
+  },
+  jianPlus({commit}){
+    commit('jian')
   }
 }
 export default new Vuex.Store({
-  state,
-  mutations
+  state ,
+  mutations ,
+  getters,
+  actions
 });
